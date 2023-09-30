a6e9cfff31430da9e764be1feb22ec28b8c75046
LucianRain <aidanbacon@gmail.com>
2023-09-29 17:58:10 -0700

    version switch

---
+++ Assets/ShoppingCartGame/PlayerController.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] float moveSpeed, turnSpeed;
+
+    private void Start()
+    {
+        
+    }
+
+    private void Update()
+    {
+        
+    }
+}
